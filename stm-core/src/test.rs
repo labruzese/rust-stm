88f53c43bb5cd6278f2974d0d3d89b662c66dabf
Skylar Abruzese <skylar@abruzese.com>
2026-04-06 14:28:51 -0400

    remove resurved async keyword

--- stm-core/src/test.rs
+++ stm-core/src/test.rs
@@ -1,33 +1,35 @@
 //! This module contains helpers for various tests.
-//! Quite a lot of tests run operations asynchonously and need to check 
+//! Quite a lot of tests run operations async_runhonously and need to check
 //! for deadlocks. We do this by waiting a certain amount of time for completion.
 //!
 //! This module contains some helpers that simplify other tests.
 
+use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc::channel;
 
 /// Check if a function `f` terminates within a given timeframe.
 ///
 /// If the function does not terminate, it keeps a thread alive forever,
 /// so don't run too many test (preferable just one) in sequence.
-pub fn terminates<F>(duration_ms: u64, f: F) -> bool 
-where F: Send + FnOnce() -> () + 'static,
+pub fn terminates<F>(duration_ms: u64, f: F) -> bool
+where
+    F: Send + FnOnce() -> () + 'static,
 {
     terminates_async(duration_ms, f, || {})
 }
 
-/// Check if a function `f` terminates within a given timeframe, 
+/// Check if a function `f` terminates within a given timeframe,
 /// but run a second function `g` concurrently.
 ///
 /// If the function does not terminate, it keeps a thread alive forever,
 /// so don't run too many test (preferable just one) in sequence.
-pub fn terminates_async<F, G>(duration_ms: u64, f: F, g: G) -> bool 
-where F: Send + FnOnce() -> () + 'static,
-      G: FnOnce() -> ()
+pub fn terminates_async<F, G>(duration_ms: u64, f: F, g: G) -> bool
+where
+    F: Send + FnOnce() -> () + 'static,
+    G: FnOnce() -> (),
 {
-    async(duration_ms, f, g).is_some()
+    async_run(duration_ms, f, g).is_some()
 }
 
 /// Run two functions `f` and `g` concurrently.
@@ -37,10 +39,11 @@ where F: Send + FnOnce() -> () + 'static,
 ///
 /// If `f` does not terminate, it keeps a thread alive forever,
 /// so don't run too many test (preferable just one) in sequence.
-pub fn async<T, F, G>(duration_ms: u64, f: F, g: G) -> Option<T>
-where F: Send + FnOnce() -> T + 'static,
-      G: FnOnce() -> (),
-      T: Send + 'static,
+pub fn async_run<T, F, G>(duration_ms: u64, f: F, g: G) -> Option<T>
+where
+    F: Send + FnOnce() -> T + 'static,
+    G: FnOnce() -> (),
+    T: Send + 'static,
 {
     let (tx, rx) = channel();
 
@@ -49,18 +52,18 @@ where F: Send + FnOnce() -> T + 'static,
         // wakeup other thread
         let _ = tx.send(t);
     });
-    
+
     g();
-    
-    if let a@Some(_) = rx.try_recv().ok() {
+
+    if let a @ Some(_) = rx.try_recv().ok() {
         return a;
     }
 
     // Give enough time for travis to get up.
     // Sleep in 50 ms steps, so that it does not waste too much time if the thread finishes earlier.
-    for _ in 0..duration_ms/50 {
+    for _ in 0..duration_ms / 50 {
         thread::sleep(Duration::from_millis(50));
-        if let a@Some(_) = rx.try_recv().ok() {
+        if let a @ Some(_) = rx.try_recv().ok() {
             return a;
         }
     }
